59c26ad73e90372466769de23f7e7e15aebccf3a
Joe James <Joe.James@softwire.com>
2018-09-26 15:54:34 +0100

    Fixed query for user books

--- src/main/java/org/softwire/training/bookish/services/UserBookService.java
+++ src/main/java/org/softwire/training/bookish/services/UserBookService.java
@@ -20,11 +20,11 @@ public class UserBookService
 
     public List<BookingQuery> getAllBookings(int userId) {
         List<BookingQuery> bookings = jdbi.withHandle(handle ->
-            handle.createQuery("SELECT copies.copyId, bookings.userId, books.title, books.author, books.isbn, copies.barcode " +
+            handle.createQuery("SELECT copies.copyId, bookings.userId, books.title, books.author, books.isbn, copies.barcode, bookings.dateTaken, bookings.dateDue " +
                     "FROM bookish.copies " +
                     "JOIN bookish.books ON books.bookId = copies.bookId " +
                     "LEFT JOIN bookish.bookings ON copies.copyId = bookings.copyId " +
-                    "WHERE books.bookId = :id " +
+                    "WHERE bookings.userId = :id " +
                     "ORDER BY books.author, books.title, copies.barcode"
                     )
                     .bind("id", userId)
